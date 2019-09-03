32f28133bae516bdf1e7b4e927757be694585393
ottagit <ottacc66@gmail.com>
2019-09-03 16:47:37 +0300

    return odd numbers

--- basic_functions.js
+++ basic_functions.js
@@ -11,3 +11,9 @@ function sumEven() {
 }
 // log out return value of function
 console.log(sumEven());
+
+function returnOdd() {
+  return num_arr.filter(e => !(e % 2 === 0));
+}
+
+console.log(returnOdd());
